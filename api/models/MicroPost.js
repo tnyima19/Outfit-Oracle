f2c17b004a1cc9dbc0477973355f9b5c010158fd
Tenzing Nyima <tnyima19@gmail.com>
2023-10-29 20:20:09 -0400

    "first commit"

---
+++ api/models/MicroPost.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class MicroPost extends Model {}
+
+  MicroPost.init(
+    {
+      content: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [3, 250],
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "MicroPost",
+    }
+  );
+
+  MicroPost.associate = (models) => {
+    // associations can be defined here
+  };
+
+  return MicroPost;
+};
---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const htmlContainer = document.getElementById("root");
+const root = ReactDOM.createRoot(htmlContainer);
+
+root.render(<App />);
