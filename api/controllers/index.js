65a495d8f772bf8f5f03f97b4d01f26de58059b1
Shashi Sugrim <ssugrim1430@bths.edu>
2023-11-14 00:45:30 -0500

    basic get products route

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 // Load each controller
 const microPostsController = require("./microPosts.js");
+const productControllerInit = require('./productController.js');
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
 router.use("/micro_posts", microPostsController);
-
+router.use("/products", productControllerInit);
 module.exports = router;
