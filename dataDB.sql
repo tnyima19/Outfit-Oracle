3d3d14545f91ecdb5a058f889b09beaad6da0d58
Shashi Sugrim <ssugrim1430@bths.edu>
2023-11-14 00:49:28 -0500

    name mismatch fix

--- dataDB.sql
+++ dataDB.sql
@@ -1,42 +1,29 @@
--- DB NAME: outfitoracleDB
-CREATE TABLE users (
-    email VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255) NOT NULL
-);
+-- Insert example data into Users
+INSERT INTO users (email, password) VALUES 
+('user1@example.com', 'password1'),
+('user2@example.com', 'password2');
 
-CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    description VARCHAR(255),
-    title VARCHAR(255),
-    category VARCHAR(255)[],
-    styles VARCHAR(255)[],
-    email VARCHAR(255) REFERENCES users(email) ON DELETE CASCADE
-);
+-- Insert example data into Products
+INSERT INTO products (description, title, category, styles, email) VALUES 
+('A cool t-shirt', 'Cool Shirt', ARRAY['clothing', 'casual'], ARRAY['modern', 'summer'], 'user1@example.com'),
+('An awesome pair of sneakers', 'Awesome Sneakers', ARRAY['footwear'], ARRAY['sporty', 'trendy'], 'user2@example.com');
 
-CREATE TABLE cart (
-    email VARCHAR(255) REFERENCES users(email) ON DELETE CASCADE,
-    product_id INT REFERENCES products(id) ON DELETE CASCADE,
-    quantity INT NOT NULL CHECK (quantity > 0),
-    PRIMARY KEY (email, product_id)
-);
+-- Insert example data into Cart
+INSERT INTO cart (email, product_id, quantity) VALUES 
+('user1@example.com', 1, 2),
+('user2@example.com', 2, 1);
 
-CREATE TABLE explore (
-    id INT REFERENCES products(id) ON DELETE CASCADE
-);
+-- Insert example data into Explore
+INSERT INTO explore (id) VALUES 
+(1),
+(2);
 
-CREATE TABLE selling (
-    email VARCHAR(255) REFERENCES users(email) ON DELETE CASCADE,
-    product_id INT REFERENCES products(id) ON DELETE CASCADE,
-    quantity INT NOT NULL CHECK (quantity > 0),
-    price INT NOT NULL CHECK (price >= 1),
-    PRIMARY KEY(email, product_id, quantity)
-);
+-- Insert example data into Selling
+INSERT INTO selling (email, product_id, quantity, price) VALUES 
+('user1@example.com', 1, 5, 20),
+('user2@example.com', 2, 3, 50);
 
-CREATE TABLE sold (
-    buyer_email VARCHAR(255) REFERENCES users(email) ON DELETE CASCADE,
-    seller_email VARCHAR(255) REFERENCES users(email) ON DELETE CASCADE,
-    date_sold TIMESTAMP,
-    product_id INT REFERENCES products(id) ON DELETE CASCADE,
-    quantity INT NOT NULL CHECK (quantity > 0),
-    PRIMARY KEY(buyer_email, seller_email, date_sold, product_id, quantity)
-);
+-- Insert example data into Sold
+INSERT INTO sold (buyer_email, seller_email, date_sold, product_id, quantity) VALUES 
+('user2@example.com', 'user1@example.com', '2023-01-01 10:00:00', 1, 1),
+('user1@example.com', 'user2@example.com', '2023-01-02 15:30:00', 2, 1);
