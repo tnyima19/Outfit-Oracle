5396dab0f939a5daf6a6a5ba24c7c3fd8ea2e52f
Shashi Sugrim <ssugrim1430@bths.edu>
2023-11-15 14:54:27 -0500

    added users route

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 // Load each controller
 const microPostsController = require("./microPosts.js");
-const productControllerInit = require('./productController.js');
-
+const productControllerInit = require("./productController.js");
+const userController = require("./userController.js");
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
 router.use("/micro_posts", microPostsController);
 router.use("/products", productControllerInit);
+router.use("/users", userController);
 module.exports = router;
