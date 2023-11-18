49c9816e9501dbec1b25998adff0175288923996
Shashi Sugrim <ssugrim1430@bths.edu>
2023-11-18 14:01:20 -0500

    getExplore route

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const { Product } = require('../models'); 
+const { Product } = require("../models");
+const { Explore } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
     // const newProduct = await Product.create({ title: "Test Product" /* other fields */ });
     const products = await Product.findAll();
-    console.log("products are: " + JSON.stringify(products))
+    console.log("products are: " + JSON.stringify(products));
     res.json(products);
   } catch (error) {
     console.error("Error in /products route", error);
     res.status(500).send("Error testing Products model");
+  }
+});
+
+router.get("/explore", async (req, res) => {
+  try {
+    const products = await Explore.findAll();
+    res.json(products);
+  } catch (error) {
+    res.status(500).send("error testing explore model");
   }
 });
 
