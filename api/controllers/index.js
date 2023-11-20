83667bdc82ecac07b745cc4b1b02710cef5d1da6
Shashi Sugrim <ssugrim1430@bths.edu>
2023-11-19 23:57:22 -0500

    createaccount route works but not integrated with db

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+const productControllerInit = require("./productController");
+const userController = require("./userController");
 // Load each controller
-const microPostsController = require("./microPosts.js");
-const productControllerInit = require("./productController.js");
-const userController = require("./userController.js");
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
-router.use("/micro_posts", microPostsController);
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[0];
+  console.log("user wants to come but token is " + authHeader);
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+// router.use("/micro_posts", microPostsController);
 router.use("/products", productControllerInit);
 router.use("/users", userController);
 module.exports = router;
--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,11 +1,71 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 
 const { Users } = require("../models");
+
+const loginUser = async (req, res) => {
+  let email = req.body.email;
+  let password = "";
+  console.log("email is " + email);
+  // const userExists = await pool.query(queries.checkUserExists, [email]);
+  // if (userExists.rowCount == 0) {
+  //   console.log("This user does not exist");
+  //   return res.send("this user does not exist");
+  // } else {
+  //   password = userExists.rows[0].password;
+  // }
+
+  console.log("password is " + password);
+  console.log("req body password: " + req.body.password);
+
+  if (await bcrypt.compare(req.body.password, password)) {
+    console.log("bcrypt says this is a good password");
+
+    const user = { email: email };
+    const accessToken = generateAccessToken(user);
+    return res.json({ accessToken: accessToken });
+
+    // return res.send("success");
+  } else {
+    console.log("bcrypt says this is NOT a good password");
+
+    return res.send("not allowed");
+  }
+};
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+}
+
+const createAccount = async (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  // const userExists = await pool.query(queries.checkUserExists, [email]);
+  // if (userExists.rowCount > 0) {
+  //   console.log("email exists");
+  //   res.status(404).send("email exists");
+  // } else {
+  //   const salt = await bcrypt.genSalt();
+  //   const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  //   await pool.query(queries.addUser, [`${email}`, `${hashedPassword}`]);
+  //   console.log("encrypted password and added to db");
+  //   res.status(201).send("encrypted password and added to db");
+  // }
+
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  console.log("hashed password " + hashedPassword);
+  console.log("encrypted password and added to db");
+  res.status(201).send("encrypted password and added to db");
+};
 
 router.get("/", async (req, res) => {
   const users = await Users.findAll();
   res.json(users);
 });
+
+router.post("/createAccount", createAccount);
 
 module.exports = router;
