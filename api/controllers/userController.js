c0e67631f026f4f2849de651b7dee56d19f9d23e
Shashi Sugrim <ssugrim1430@bths.edu>
2023-11-23 11:50:31 -0500

    working login/creation of accounts with connection to db

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -2,20 +2,19 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 require("dotenv").config();
-
+const jwt = require('jsonwebtoken');
 const { Users } = require("../models");
 
 const loginUser = async (req, res) => {
   let email = req.body.email;
-  let password = "";
   console.log("email is " + email);
-  // const userExists = await pool.query(queries.checkUserExists, [email]);
-  // if (userExists.rowCount == 0) {
-  //   console.log("This user does not exist");
-  //   return res.send("this user does not exist");
-  // } else {
-  //   password = userExists.rows[0].password;
-  // }
+  const userExists = await Users.findOne({where: {email:email}});
+  if (userExists == null) {
+    console.log("This user does not exist");
+    return res.send("this user does not exist");
+  } else {
+    password = userExists.password;
+  }
 
   console.log("password is " + password);
   console.log("req body password: " + req.body.password);
@@ -25,13 +24,13 @@ const loginUser = async (req, res) => {
 
     const user = { email: email };
     const accessToken = generateAccessToken(user);
-    return res.json({ accessToken: accessToken });
+    return res.status(200).json({ accessToken: accessToken });
 
     // return res.send("success");
   } else {
     console.log("bcrypt says this is NOT a good password");
 
-    return res.send("not allowed");
+    return res.status(401).send("wrong password");
   }
 };
 
@@ -43,22 +42,21 @@ const createAccount = async (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
   // const userExists = await pool.query(queries.checkUserExists, [email]);
-  // if (userExists.rowCount > 0) {
-  //   console.log("email exists");
-  //   res.status(404).send("email exists");
-  // } else {
-  //   const salt = await bcrypt.genSalt();
-  //   const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  //   await pool.query(queries.addUser, [`${email}`, `${hashedPassword}`]);
-  //   console.log("encrypted password and added to db");
-  //   res.status(201).send("encrypted password and added to db");
-  // }
+  const userExists = await Users.findOne({where: {email:email}});
+  console.log("user exists returning:  " + userExists);
+  if (userExists != null) {
+    console.log("email exists");
+    return res.status(404).send("email exists");
+  } else {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    // await pool.query(queries.addUser, [`${email}`, `${hashedPassword}`]);
+    Users.create({email:email, password:hashedPassword})
+    console.log("encrypted password and added to db");
+    return res.status(201).send("encrypted password and added to db");
+  }
 
-  const salt = await bcrypt.genSalt();
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  console.log("hashed password " + hashedPassword);
-  console.log("encrypted password and added to db");
-  res.status(201).send("encrypted password and added to db");
+
 };
 
 router.get("/", async (req, res) => {
@@ -67,5 +65,6 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/createAccount", createAccount);
+router.post("/loginUser", loginUser);
 
 module.exports = router;
