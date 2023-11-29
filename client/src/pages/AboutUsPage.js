68149e91e31d96454d70a2dbed93b3710e056301
cherry-code <123757698+cherry-code@users.noreply.github.com>
2023-11-29 12:55:29 -0500

    Update AboutUsPage.js

--- client/src/pages/AboutUsPage.js
+++ client/src/pages/AboutUsPage.js
@@ -1,44 +1,55 @@
 import React from "react";
+import "./AboutUs.css";
 
 function AboutUsPage(props) {
   return (
     <>
-      <div className="col text-center">
-        <h2 className="mb-3">About our project</h2>
-        <p className="mb-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-          quidem adipisci nobis quia eum quaerat quos ducimus, deleniti
-          exercitationem animi itaque iste illo reiciendis vitae atque
-          necessitatibus voluptatum repellendus quisquam?
-        </p>
-        <h2 className="mb-3">About our Team</h2>
-        <div className="row">
-          <div className="col-lg-4">
-            <h3>Firstname Lastname</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Dignissimos in itaque nihil consectetur qui natus similique
-              nostrum molestias, ipsa explicabo hic impedit aspernatur. Ipsa
-              provident neque culpa alias incidunt amet.
-            </p>
-          </div>
-          <div className="col-lg-4">
-            <h3>Firstname Lastname</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Dignissimos in itaque nihil consectetur qui natus similique
-              nostrum molestias, ipsa explicabo hic impedit aspernatur. Ipsa
-              provident neque culpa alias incidunt amet.
-            </p>
-          </div>
-          <div className="col-lg-4">
-            <h3>Firstname Lastname</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Dignissimos in itaque nihil consectetur qui natus similique
-              nostrum molestias, ipsa explicabo hic impedit aspernatur. Ipsa
-              provident neque culpa alias incidunt amet.
-            </p>
+      <div className="main-text">
+        <div className="project-summary">
+          <h2 className="mb-3">About Our Project</h2>
+          <p className="mb-5">
+            We wish to create a space for shoppers like you to spend their time efficiently and make their favorite hobby easier by personalizing experiences. We also wish to inspire and encourage others to take part who have not gotten into shopping due to the inconveniences that come along with it. With our style quizzes, we will recommend the right clothes for you and make your search shorter. 
+          </p>
+        </div>
+        <div className="team-summary">
+          <h2 className="mb-3">About Our Team</h2>
+          <div className="row">
+            <div className="col-lg-6">
+              <h3>Firstname Lastname</h3>
+              <p className="mb-5">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Dignissimos in itaque nihil consectetur qui natus similique
+                nostrum molestias, ipsa explicabo hic impedit aspernatur. Ipsa
+                provident neque culpa alias incidunt amet.
+              </p>
+            </div>
+            <div className="col-lg-6">
+              <h3>Firstname Lastname</h3>
+              <p className="mb-5">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Dignissimos in itaque nihil consectetur qui natus similique
+                nostrum molestias, ipsa explicabo hic impedit aspernatur. Ipsa
+                provident neque culpa alias incidunt amet.
+              </p>
+            </div>
+            <div className="col-lg-6">
+              <h3>Firstname Lastname</h3>
+              <p className="mb-5">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Dignissimos in itaque nihil consectetur qui natus similique
+                nostrum molestias, ipsa explicabo hic impedit aspernatur. Ipsa
+                provident neque culpa alias incidunt amet.
+              </p>
+            </div>
+            <div className="col-lg-6">
+              <h3>Firstname Lastname</h3>
+              <p className="mb-5">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Dignissimos in itaque nihil consectetur qui natus similique
+                nostrum molestias, ipsa explicabo hic impedit aspernatur. Ipsa
+                provident neque culpa alias incidunt amet.
+              </p>
+            </div>
           </div>
         </div>
       </div>
